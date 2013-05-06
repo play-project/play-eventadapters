2f284cc277ef618ee7f082fdc97941babfd36675
ningyuan pan <ny_pan@hotmail.com>
2013-05-06 19:32:50 +0200

    RESTful client mit jersey 1.8

--- play-eventadapter-abstractrdfsender-rest/src/main/java/eu/play_project/play_eventadapter/AbstractSenderRest.java
+++ play-eventadapter-abstractrdfsender-rest/src/main/java/eu/play_project/play_eventadapter/AbstractSenderRest.java
@@ -1,18 +1,34 @@
 package eu.play_project.play_eventadapter;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.ws.rs.core.MultivaluedMap;
 import javax.xml.namespace.QName;
 
 import org.event_processing.events.types.Event;
 import org.ontoware.rdf2go.model.Model;
 import org.w3c.dom.Element;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+
+
+
 import eu.play_project.play_commons.constants.Constants;
 import eu.play_project.play_commons.eventtypes.EventHelpers;
 
 public class AbstractSenderRest {
-
+	
+	static{
+		client = Client.create();
+	}
+	
+	/** global RESTful client*/
+	private static Client client;
+	
 	/** Default REST endpoint for notifications */
 	private String notifyEndpoint = Constants.getProperties().getProperty(
 			"play.platform.endpoint") + "publish";
@@ -23,7 +39,7 @@ public class AbstractSenderRest {
 	private final Logger logger = Logger.getAnonymousLogger();
 	private QName defaultTopic;
 	private Boolean online = true;
-
+	
 	public AbstractSenderRest(QName defaultTopic) {
 		this.defaultTopic = defaultTopic;
 	}
@@ -72,6 +88,23 @@ public class AbstractSenderRest {
 	public void notify(String notifPayload, QName topicUsed) {
 		
 		// TODO ningyuan
+		MultivaluedMap data = new MultivaluedMapImpl();
+		// what is topicUsed
+		data.add("resource", eu.play_project.play_commons.constants.Stream.FacebookStatusFeed.getTopicUri()+"#stream");
+		data.add("message", notifPayload);
+			//System.out.println("Endpoint: "+notifyEndpoint);
+			//System.out.println("Topic: "+eu.play_project.play_commons.constants.Stream.FacebookStatusFeed.getTopicUri()+"#stream");
+		WebResource wr = client.resource(notifyEndpoint);
+			//System.out.println("Token: "+PLAY_PLATFORM_APITOKEN);
+		ClientResponse response = wr.header("Authorization", "Bearer " + PLAY_PLATFORM_APITOKEN)
+		  .type("application/x-www-form-urlencoded")
+		  .post(ClientResponse.class, data);
+		
+		//TODO response state 200 201 202?
+		if(response.getStatus() != 200){
+			logger.log(Level.SEVERE, "No event was notified because of response status "+response.getStatus());
+		}
+			System.out.println("Response status: "+response.getStatus());
 	}
 
 	/**
--- play-eventadapter-abstractrdfsender-rest/src/test/java/eu/play_project/play_eventadapter/tests/AbstractSenderRestTest.java
+++ play-eventadapter-abstractrdfsender-rest/src/test/java/eu/play_project/play_eventadapter/tests/AbstractSenderRestTest.java
@@ -1,16 +1,26 @@
 package eu.play_project.play_eventadapter.tests;
 
+import static eu.play_project.play_commons.constants.Stream.SituationalEventStream;
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import eu.play_project.play_commons.constants.Constants;
+import eu.play_project.play_eventadapter.AbstractSenderRest;
 
 public class AbstractSenderRestTest {
 
-	/**
-	 * Confirm that important configuration properties for event sending are
-	 * set.
-	 */
+	private AbstractSenderRest eventSource;
+
+	@Before
+	public void setup() {
+		eventSource = new AbstractSenderRest(SituationalEventStream.getTopicQName());
+		// Only for unit testing:
+		eventSource.setNoNetworking(true);
+	}
+	
 	@Test
 	public void testConfigurationProperties() {
 		Assert.assertNotNull(Constants.getProperties("play-eventadapter.properties").getProperty(
@@ -18,4 +28,11 @@ public class AbstractSenderRestTest {
 		Assert.assertNotNull(Constants.getProperties().getProperty(
 				"play.platform.endpoint"));
 	}
+	
+	@Test
+	public void testNotifyStringQName() {
+		eventSource.notify("{'foo' : 'bar'}", null);
+		assertTrue(true);
+	}
+
 }
