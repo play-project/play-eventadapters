14b48e79563e5fcff22e557965f81817cf42f19f
stuehmer <roland.stuehmer@fzi.de>
2013-10-22 14:21:13 +0200

    Add manual test for diagnosing user problems

--- play-eventadapter-abstractrdfsender-rest/src/test/java/eu/play_project/play_eventadapter/tests/AbstractSenderRestTest.java
+++ play-eventadapter-abstractrdfsender-rest/src/test/java/eu/play_project/play_eventadapter/tests/AbstractSenderRestTest.java
@@ -3,10 +3,13 @@ package eu.play_project.play_eventadapter.tests;
 
 import static org.junit.Assert.assertTrue;
 
+import org.event_processing.events.types.CrisisSituationEvent;
+import org.event_processing.events.types.Event;
 import org.junit.Before;
 import org.junit.Test;
 
 import eu.play_project.play_commons.constants.Stream;
+import eu.play_project.play_commons.eventtypes.EventHelpers;
 import eu.play_project.play_eventadapter.AbstractSenderRest;
 
 public class AbstractSenderRestTest {
@@ -24,6 +27,20 @@ public class AbstractSenderRestTest {
 	public void testNotify() {
 		eventSource.notify("");
 		assertTrue(true);
+	}
+	
+	/**
+	 * Manual test to send a authenticated notification to a real online
+	 * endpoint. (Implying this test has remote sided effects.)
+	 */
+	public static void main(String[] args) {
+		AbstractSenderRest sender = new AbstractSenderRest(Stream.SituationalEventStream.getTopicQName(), "http://play.inria.fr:8080/play/api/v1/platform/publish");
+		Event e = EventHelpers.builder()
+				.type(CrisisSituationEvent.RDFS_CLASS)
+				.stream(Stream.SituationalEventStream)
+				.build();
+		sender.notify(e);
+
 	}
 
 }
